14fde737aa94d619cea84deebaeeda31d5504e78
kuojsean <kuo.j.sean@gmail.com>
2018-01-25 09:46:40 -0700

    Fixed the for loop, added more to the array and created the console output assignment.

--- StupidProject/Controller/Controller.cpp
+++ StupidProject/Controller/Controller.cpp
@@ -17,14 +17,29 @@ Controller :: Controller()
 
 void Controller :: start()
 {
-    int intArray [] = {1, 2, 3, 4, 5};
-    double dubArray [] = {1.1, 1.2, 1.3, 1.4, 1.5};
-    string stringArray [] = {"one", "two", "three", "four", "five"};
+    string name = "Sean";
+    string *nameAddress;
+    
+    nameAddress = &name;
+    
+    cout << name << endl;
+    for (int count = 13; count < 32 ; count++)
+    {
+        cout << count << " ";
+    }
+    cout << endl;
+    cout << nameAddress << endl;
+    cout << "separator 1 " << "separator 2 " << "separator 3 " << endl;
+    
+    int intArray [] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    double dubArray [] = {1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9, 2.0};
+    string stringArray [] = {"one", "two", "three", "four", "five", "six" ,"seven" ,"eight","nine","ten"};
     
     for (int index = 0; index < 10; index ++)
     {
         cout << "This loop has executed " << index + 1 << " times" <<endl;
-        cout << intArray[index] << "" << dubArray[index] << "" << stringArray[index] << "" << endl;
+        cout << intArray[index] << " " << dubArray[index] << " " << stringArray[index] << " " << endl;
     }
     
+
 }
